fc9175769f14fc8bc17f6ddb597312a8e295c9c9
Caius Brindescu <brind@illinois.edu>
2013-04-13 20:27:27 -0500

    Cleaned the imports

--- croder/Wizard/src/edu/illinois/croder/wizard/ShowCommandHandler.java
+++ croder/Wizard/src/edu/illinois/croder/wizard/ShowCommandHandler.java
@@ -3,7 +3,6 @@ package edu.illinois.croder.wizard;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.commands.IHandlerListener;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.ui.handlers.HandlerUtil;
 
