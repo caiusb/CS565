452c14b4663098cdcb54bcf42a56b4100306afbc
cdmihai <codo@illinois.edu>
2013-04-13 23:26:39 -0500

    implemented getElements()

--- croder/CodeSelector/src/edu/illinois/codeselector/views/ViewContentProvider.java
+++ croder/CodeSelector/src/edu/illinois/codeselector/views/ViewContentProvider.java
@@ -1,26 +1,34 @@
 package edu.illinois.codeselector.views;
 
+import java.util.ArrayList;
+
 import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.Viewer;
+
+import edu.illinois.codeselector.models.Snippet;
 
 public class ViewContentProvider implements IStructuredContentProvider {
 
 	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public Object[] getElements(Object inputElement) {
-		// TODO Auto-generated method stub
-		return null;
+		if (inputElement instanceof ArrayList<?>) {
+			ArrayList<Snippet> snippets = ((ArrayList<Snippet>) inputElement);
+			return snippets.toArray(new Snippet[snippets.size()]);
+		}
+
+		return new Snippet[0];
 	}
 
 }
