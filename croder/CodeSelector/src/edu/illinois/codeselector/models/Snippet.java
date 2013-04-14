fefdd17f4e809f289b0ca5c541ae82720ee87cdb
cdmihai <codo@illinois.edu>
2013-04-13 22:25:06 -0500

    added dump model

---
+++ croder/CodeSelector/src/edu/illinois/codeselector/models/Snippet.java
@@ -0,0 +1,15 @@
+package edu.illinois.codeselector.models;
+
+public class Snippet {
+	private Object snippet;
+	
+	public Snippet(Object snippet){
+		this.snippet = snippet;
+	}
+	
+	@Override
+	public String toString() {
+		return snippet.toString();
+	}
+	
+}
