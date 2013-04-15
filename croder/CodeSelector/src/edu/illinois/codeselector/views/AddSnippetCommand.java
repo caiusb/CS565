be750262b2fb6366649532f85e6a41d745b018f9
cdmihai <codo@illinois.edu>
2013-04-14 22:29:07 -0500

    Added a command to all popup menus to add pieces of code entities to a review

---
+++ croder/CodeSelector/src/edu/illinois/codeselector/views/AddSnippetCommand.java
@@ -0,0 +1,61 @@
+package edu.illinois.codeselector.views;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.text.TextSelection;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.TreeSelection;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.ISelectionService;
+import org.eclipse.ui.PlatformUI;
+
+import edu.illinois.codeselector.views.exceptions.UnknownSelectionException;
+
+public class AddSnippetCommand extends AbstractHandler {
+
+	
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		ISelectionService ss = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService();
+		ISelection selection = ss.getSelection();
+		
+		try {
+			Object selectionObject = getSelectionObject(selection);
+			displayMessage(selectionObject.getClass().getName() + "\n" + selectionObject.toString());
+			handleSelectionObject(selectionObject);
+		} catch (UnknownSelectionException e) {
+			displayMessage("Cannot handle exception of type " + e.getMessage());
+		}
+		
+		return null;
+	}
+
+	private void handleSelectionObject(Object selectionObject) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private Object getSelectionObject(ISelection selection) throws UnknownSelectionException {
+		if (selection instanceof TreeSelection){
+			TreeSelection treeSelection = (TreeSelection) selection;
+			
+			return treeSelection.getFirstElement();
+		}
+		
+		else if (selection instanceof TextSelection){
+			TextSelection textSelection = (TextSelection) selection;
+			
+			return textSelection.getText();
+		}
+		
+		throw new UnknownSelectionException(selection.getClass().getName());
+	}
+
+	private void displayMessage(String message) {
+		MessageDialog.openInformation(Display.getCurrent().getActiveShell(),
+				"CodeSelector", message);
+	}
+
+}
