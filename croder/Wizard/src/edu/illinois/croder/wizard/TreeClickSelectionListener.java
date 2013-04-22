530129fa601c2bd70589a66a27e67ca3b29792d7
Caius Brindescu <brind@illinois.edu>
2013-04-21 20:58:12 -0500

    Added a new listener for the TreeViewer in the wizard

---
+++ croder/Wizard/src/edu/illinois/croder/wizard/TreeClickSelectionListener.java
@@ -0,0 +1,32 @@
+package edu.illinois.croder.wizard;
+
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.swt.widgets.Text;
+
+import edu.illinois.codeselector.models.snippets.AbstractSnippet;
+
+public class TreeClickSelectionListener implements ISelectionChangedListener {
+
+	private Text text;
+	private Text code;
+
+	public TreeClickSelectionListener(Text text, Text code) {
+		this.text = text;
+		this.code = code;
+	}
+
+	@Override
+	public void selectionChanged(SelectionChangedEvent event) {
+		Object eventSource = event.getSource();
+		if (!(eventSource instanceof TreeViewer))
+			return;
+
+		AbstractSnippet selectedElement = (AbstractSnippet) ((IStructuredSelection) ((TreeViewer) eventSource)
+				.getSelection()).getFirstElement();
+		code.setText(selectedElement.getCode());
+	}
+
+}
