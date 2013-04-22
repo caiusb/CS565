690acdbfa9980735ebabbadc9de1117123c40662
Caius Brindescu <brind@illinois.edu>
2013-04-21 20:25:47 -0500

    Added a getSignature method to the Snippet
    So I can show a small version in the TreeThing

--- croder/CodeSelector/src/edu/illinois/codeselector/models/snippets/AbstractSnippet.java
+++ croder/CodeSelector/src/edu/illinois/codeselector/models/snippets/AbstractSnippet.java
@@ -2,6 +2,9 @@ package edu.illinois.codeselector.models.snippets;
 
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaModelException;
 
 import edu.illinois.codeselector.views.exceptions.UnknownSelectionException;
 
@@ -19,6 +22,23 @@ public abstract class AbstractSnippet {
 		return javaElementForSnippet;
 	}
 	
+	public String getSignature() {
+		IJavaElement javaElement = getJavaElementForSnippet();
+		int elementType = javaElement.getElementType();
+		switch (elementType) {
+		case IJavaElement.METHOD:
+			IMethod method = (IMethod) javaElement;
+			try {
+				return method.getSignature();
+			} catch (JavaModelException e) {
+			}
+		case IJavaElement.TYPE:
+			IType type = (IType) javaElement;
+			return type.getFullyQualifiedName();
+		}
+		return javaElement.getElementName();
+	}
+
 	public static AbstractSnippet constructSnippetForTarget(Object snippetTarget, ICompilationUnit activeICU) throws UnknownSelectionException{
 		if (snippetTarget instanceof IJavaElement){
 			return new JavaElementSnippet((IJavaElement)snippetTarget);
