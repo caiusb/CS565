af1a499d4f7788431d471036cdcf3403483ca051
Caius Brindescu <brind@illinois.edu>
2013-04-22 14:02:24 -0500

    Added listeners for the text fields

---
+++ croder/Wizard/src/edu/illinois/croder/wizard/PreviewModifyListener.java
@@ -0,0 +1,34 @@
+package edu.illinois.croder.wizard;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.widgets.Text;
+
+import edu.illinois.codeselector.models.snippets.Snippet;
+
+public abstract class PreviewModifyListener implements ModifyListener {
+
+	private TreeViewer treeViewer;
+
+	public PreviewModifyListener(TreeViewer treeViewer) {
+		this.treeViewer = treeViewer;
+	}
+
+	@Override
+	public void modifyText(ModifyEvent e) {
+		if (!(e.getSource() instanceof Text))
+			return;
+
+		Text text = (Text) e.getSource();
+		String content = text.getText();
+
+		Snippet selected = ((Snippet) ((IStructuredSelection) treeViewer.getSelection())
+				.getFirstElement());
+		
+		setProperty(content, selected);
+	}
+
+	protected abstract void setProperty(String content, Snippet selected);
+}
