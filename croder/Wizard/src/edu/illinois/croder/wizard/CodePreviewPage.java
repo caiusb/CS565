ca7e9119b99d872cfd8f239ea7988c32378ac69c
Caius Brindescu <brind@illinois.edu>
2013-04-22 19:09:35 -0500

    Moved the title to the new page

--- croder/Wizard/src/edu/illinois/croder/wizard/CodePreviewPage.java
+++ croder/Wizard/src/edu/illinois/croder/wizard/CodePreviewPage.java
@@ -25,7 +25,6 @@ public class CodePreviewPage extends WizardPage {
 		composite.setLayout(new GridLayout(1, false));
 		composite.setLayoutData(new GridData(GridData.FILL_BOTH));
 		
-		makeTitle(composite);
 		makeCodeArea(composite);
 		
 		setControl(composite);
@@ -73,19 +72,4 @@ public class CodePreviewPage extends WizardPage {
 		treeViewer.getControl().setLayoutData(new GridData(GridData.FILL_BOTH));
 		return treeViewer;
 	}
-
-	private void makeTitle(Composite composite) {
-		Composite titleComposite = new Composite(composite,SWT.NONE);
-		titleComposite.setLayout(new GridLayout(2, false));
-		titleComposite.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		Label label = new Label(titleComposite, SWT.NONE);
-		label.setText("Title of your task:");
-		
-		Composite titleTextComposite = new Composite(titleComposite, SWT.NONE);
-		titleTextComposite.setLayout(new GridLayout(1, true));
-		titleTextComposite.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		Text titleField = new Text(titleTextComposite, SWT.BORDER);
-		titleField.setLayoutData(new GridData(GridData.FILL_BOTH));
-	}
-
 }
--- croder/Wizard/src/edu/illinois/croder/wizard/TaskSetupPage.java
+++ croder/Wizard/src/edu/illinois/croder/wizard/TaskSetupPage.java
@@ -2,7 +2,11 @@ package edu.illinois.croder.wizard;
 
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 
 public class TaskSetupPage extends WizardPage {
 
@@ -15,8 +19,26 @@ public class TaskSetupPage extends WizardPage {
 	@Override
 	public void createControl(Composite parent) {
 		Composite composite = new Composite(parent, SWT.NONE);
+		composite.setLayout(new GridLayout(1, true));
+		composite.setLayoutData(new GridData(GridData.FILL_BOTH));
+		
+		makeTitle(composite);
 		
 		setControl(composite);
+	}
+
+	private void makeTitle(Composite composite) {
+		Composite titleComposite = new Composite(composite,SWT.NONE);
+		titleComposite.setLayout(new GridLayout(2, false));
+		titleComposite.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		Label label = new Label(titleComposite, SWT.NONE);
+		label.setText("Title of your task:");
+		
+		Composite titleTextComposite = new Composite(titleComposite, SWT.NONE);
+		titleTextComposite.setLayout(new GridLayout(1, true));
+		titleTextComposite.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		Text titleField = new Text(titleTextComposite, SWT.BORDER);
+		titleField.setLayoutData(new GridData(GridData.FILL_BOTH));
 	}
 
 }
