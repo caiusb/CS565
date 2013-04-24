ff417dfa085528141672aa98ecf49320dc5228f0
Semih Okur <semih@Semihs-MacBook-Pro.local>
2013-04-24 18:11:41 -0500

    good

--- croder/ReviewBrowser/src/edu/illinois/stackexchange/WebAPI.java
+++ croder/ReviewBrowser/src/edu/illinois/stackexchange/WebAPI.java
@@ -1,7 +1,11 @@
 package edu.illinois.stackexchange;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -9,9 +13,13 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 
+import com.DeathByCaptcha.Captcha;
+import com.DeathByCaptcha.Client;
+import com.DeathByCaptcha.SocketClient;
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
 import com.gargoylesoftware.htmlunit.html.HtmlTextArea;
@@ -19,11 +27,25 @@ import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
 
 public class WebAPI implements WebApiInterface {
 
-	WebClient webClient = new WebClient();
-	HtmlPage page = null;
+	WebClient webClient ;
+	HtmlPage page;
+	
+	public WebAPI()
+	{
+		webClient = new WebClient();
+		page=null;
+		webClient.getOptions().setJavaScriptEnabled(false);
+	}
 
-	
-	
+	public static void main(String[] args)
+	{
+		WebAPI test= new WebAPI();
+		test.login("semih.okur", "Ker32nel");
+		
+		String title = "Database Design process when using Hibernate";
+		String content= "My question is, when using ORM frameworks like Hibernate, does it makes sense for the App Developer to work with the person doing the database design?";
+		test.postQuestion(title, content);
+	}
 	
 	public void login(String email, String pass) {
 		webClient.getOptions().setThrowExceptionOnScriptError(false);
@@ -33,9 +55,9 @@ public class WebAPI implements WebApiInterface {
 
 			((HtmlTextInput) page.getElementById("openid_identifier"))
 					.setValueAttribute("https://www.google.com/accounts/o8/id");
-			;
+			
 			page = ((HtmlElement) page.getElementById("submit-button")).click();
-			;
+			
 			((HtmlTextInput) page.getElementById("Email"))
 					.setValueAttribute(email);
 			((HtmlPasswordInput) page.getElementById("Passwd"))
@@ -43,7 +65,7 @@ public class WebAPI implements WebApiInterface {
 			page = ((HtmlElement) page.getElementById("signIn")).click();
 
 			System.out.println(page.getWebResponse().getContentAsString()
-					.contains("aldur"));
+					.contains("user244"));
 
 		} catch (FailingHttpStatusCodeException e1) {
 			System.out.println("FailingHttpStatusCodeException thrown:"
@@ -67,15 +89,50 @@ public class WebAPI implements WebApiInterface {
 
 		try {
 			page = webClient.getPage("http://codereview.stackexchange.com/questions/ask");
+			
+	
 			((HtmlTextInput) page.getElementById("title"))
 					.setValueAttribute(title);
 			((HtmlTextArea) page.getElementById("wmd-input"))
 					.setTextContent(content);
+			
+			
+            HtmlTextInput textField3 = (HtmlTextInput) page.getElementById("tagnames");
+            textField3.setValueAttribute("java");
+			
 			page = ((HtmlElement) page.getElementById("submit-button")).click();
+			System.out.println(page.getFullyQualifiedUrl(""));
 			
-			System.err.println("posted questions");
+			String urlCaptcha= page.getWebResponse().getContentAsString().split("iframe src=\"")[1].split("\"")[0];
 			
-			return page.getBaseURI();
+			HtmlPage captchaPage= webClient.getPage(urlCaptcha);
+			
+			String imgUrl= "http://www.google.com/recaptcha/api/"+ captchaPage.getWebResponse().getContentAsString().split("src=\"")[1].split("\"")[0];
+			
+			Client client = (Client)(new SocketClient("kernel32","ker32nel"));
+	        client.isVerbose = true;
+	        
+	        Captcha captcha = null;
+            try {
+                captcha = client.decode(downloadImage(new URL(imgUrl)), 120);
+            } catch (IOException e) {
+                System.out.println("Failed uploading CAPTCHA");
+            }
+            if (null == captcha) {
+                System.out.println("Failed CAPTCHA");
+            }
+            
+            ((HtmlInput) captchaPage.getElementById("recaptcha_response_field")).setValueAttribute(captcha.text);
+            captchaPage = ((HtmlElement)captchaPage.getElementByName("submit")).click();
+            
+            String confirmationCode= captchaPage.getWebResponse().getContentAsString().split("<textarea")[1].split(">")[1].split("<")[0];
+			
+            ((HtmlTextArea) page.getElementByName("recaptcha_challenge_field")).setTextContent(confirmationCode);
+            
+            page= ((HtmlElement)page.getElementById("btn-captcha")).click();
+
+            return page.getFullyQualifiedUrl("").toString();
+
 
 		} catch (FailingHttpStatusCodeException e1) {
 			System.out.println("FailingHttpStatusCodeException thrown:"
@@ -136,6 +193,30 @@ public class WebAPI implements WebApiInterface {
 		return null;
 	
 		
+	}
+	
+	public byte[] downloadImage(URL url)
+	{
+		ByteArrayOutputStream bais = new ByteArrayOutputStream();
+		InputStream is = null;
+		try {
+		  is = url.openStream ();
+		  byte[] byteChunk = new byte[4096]; // Or whatever size you want to read in at a time.
+		  int n;
+
+		  while ( (n = is.read(byteChunk)) > 0 ) {
+		    bais.write(byteChunk, 0, n);
+		  }
+		  is.close();
+		}
+		catch (IOException e) {
+		  System.err.printf ("Failed while reading bytes from %s: %s", url.toExternalForm(), e.getMessage());
+		  e.printStackTrace ();
+		  // Perform any other exception handling that's appropriate.
+		}
+		finally {
+			return bais.toByteArray();
+		}
 	}
 	
 
