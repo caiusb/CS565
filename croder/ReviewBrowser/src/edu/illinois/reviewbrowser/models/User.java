81244c4f126a782918da141e817cc3effd0d6bc5
cdmihai <codo@illinois.edu>
2013-04-23 19:14:47 -0500

    added equals and hashcode to user

--- croder/ReviewBrowser/src/edu/illinois/reviewbrowser/models/User.java
+++ croder/ReviewBrowser/src/edu/illinois/reviewbrowser/models/User.java
@@ -1,6 +1,5 @@
 package edu.illinois.reviewbrowser.models;
 
-import edu.illinois.stackexchange.WebAPI;
 
 public class User {
 	private String email;
@@ -18,4 +17,38 @@ public class User {
 	public String getPass() {
 		return pass;
 	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof User){
+			User other = (User) obj;
+			
+			return other.email.equals(email) && other.pass.equals(pass);
+		}
+		
+		return false;
+	}
+	
+	@Override
+	public int hashCode() {
+		int hash = 1;
+		
+        hash = hash * 17 + email.hashCode();
+        hash = hash * 31 + pass.hashCode();
+        
+        return hash;
+	}
+	
+	public static void main(String[] args) {
+		User u1 = new User("unu", "doi");
+		User u2 = new User("unu", "doi");
+		User u3 = new User("doi", "unu");
+		
+		assert u1.equals(u2);
+		assert u1.hashCode() == u2.hashCode();
+		
+		assert !u1.equals(u3);
+		assert u1.hashCode() != u3.hashCode();
+	}
 }
+
